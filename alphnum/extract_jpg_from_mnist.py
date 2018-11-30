6dcc7924f79722e47c60cf35c189cf1797af6955
Vinh Ngu <v.ngu@mmkh.de>
2018-11-30 14:29:52 +0100

    Add script 'extract_from_mnist'.

---
+++ alphnum/extract_jpg_from_mnist.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+"""
+This script extracts the images (jpg.) from the mnist dataset provided by keras.
+"""
+
+import os
+
+import cv2
+import numpy as np
+from keras.datasets import mnist
+from tqdm import tqdm
+
+batch_size = 128
+num_classes = 10
+epochs = 12
+
+# input image dimensions
+img_rows, img_cols = 28, 28
+
+label_dirs = {
+	"0": "zero",
+	"1": "one",
+	"2": "two",
+	"3": "three",
+	"4": "four",
+	"5": "five",
+	"6": "six",
+	"7": "seven",
+	"8": "eight",
+	"9": "nine"
+}
+
+for label in label_dirs.values():
+	path = "dataset/" + label
+	if not os.path.exists(path):
+		os.makedirs(path)
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+for i in tqdm(range(60000)):
+	label = label_dirs[str(y_train[i])]
+	path = os.path.join("dataset", label)
+	nparray_inv = np.invert(np.array(x_train[i], dtype='uint8'))
+	filename = '{:05d}'.format(i) + ".jpg"
+	cv2.imwrite(os.path.join(path, filename), nparray_inv)
